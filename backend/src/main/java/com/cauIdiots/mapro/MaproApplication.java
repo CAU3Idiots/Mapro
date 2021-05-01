af77ee03b07a0786d0ec6d96aeb1219420a15180
마재완 <mah@majaewan-ui-MacBookPro.local>
2021-05-01 15:54:51 +0900

    add backend file spring-boot with gradle builder \n -spring boot with gradle builder

---
+++ backend/src/main/java/com/cauIdiots/mapro/MaproApplication.java
@@ -0,0 +1,13 @@
+package com.cauIdiots.mapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaproApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cauIdiots/mapro/MaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauIdiots.mapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
